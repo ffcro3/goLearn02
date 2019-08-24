cb1e36611f39fb0696d47edd76f903527cea96c7
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-23 21:57:40 -0300

    Migration. DB Connection. Sequelize config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-import app from "./app";
+import app from './app';
 
-app.listen(3030, () => {
-  return console.log("Application started at port 3030 ...");
-});
+// eslint-disable-next-line no-console
+app.listen(3030, () => console.log('Application started at port 3030 ...'));
