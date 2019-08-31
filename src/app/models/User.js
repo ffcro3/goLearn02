2e82ee89ad905e29ee043f8d4a53108281a4fa28
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-31 15:36:52 -0300

    Static express function to access files into uploads

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -27,7 +27,7 @@ class User extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
   }
 
   // METHOD TO CHECK IF THE CRYPTOGRAPHY IS THE SAME THAT REGISTRED IN DB
