a5cdf42f3e32a38ef7935607e0e3f7e9191913ed
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-31 19:26:58 -0300

    Schedule Controller + Schedule index

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,6 +22,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/Schedule', ScheduleController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
