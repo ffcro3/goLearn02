aad8edcbfd683660dd58477b67481f6cffdee91b
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-08-31 21:18:49 -0300

    Appointment Controller. Canceled an appointment

--- src/routes.js
+++ src/routes.js
@@ -22,11 +22,15 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/Schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
+
 routes.put('/notifications/:id', NotificationController.update);
-routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
